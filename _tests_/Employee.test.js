d0ded95eb9e5d170e082aeb9b204c8413fa23202
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-07 21:17:02 -0500

    Changed testEmployee to showcase name capitalization test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,14 +3,21 @@ const Employee = require('../lib/Employee');
 describe("Employee", () => {
   describe("Initialize", () => {
     it("should return an object when called with the 'new' keyword", () => {
-      const testEmployee = new Employee('John', '281', 'johnTestEmail@example.com');
+      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
 
       expect(typeof testEmployee).toEqual('object')
     })
-  })
+  });
+  describe('name', () => {
+    it('should return a name capitalized', () => {
+      const check = 'John Wick'
+      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
+      expect(testEmployee.name).toEqual(check)
+    });
+  });
   describe('id', () => {
     it("should return a number", () => {
-      const testEmployee = new Employee('John', '281', 'johnTestEmail@example.com');
+      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
       expect(typeof testEmployee.id).toEqual('number')
     })
   });
