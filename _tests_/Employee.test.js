2e538b9decae23a796293edbed87a323fe9f3355
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-07 23:22:47 -0500

    Moved const testEmployee to the Employee describe section to remove redundancy, changed the second test in name to fall in line with testing of other categories, added test for calling methods in both the id section and email section

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,34 +1,37 @@
 const Employee = require('../lib/Employee');
 
-describe("Employee", () => {
-  describe("Initialize", () => {
-    it("should return an object when called with the 'new' keyword", () => {
-      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
-
+describe('Employee', () => {
+  const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
+  describe('Initialization', () => {
+    it('should return an object when called with the "new" keyword', () => {
       expect(typeof testEmployee).toEqual('object');
-    })
+    });
   });
   describe('name', () => {
     it('should return a string', () => {
-      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
       expect(typeof testEmployee.name).toEqual('string');
     });
-    it('should return a name capitalized', () => {
+    it('calling getName() should return each name capitalized', () => {
       const check = 'John Wick';
-      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
       expect(testEmployee.name).toEqual(check);
     });
   });
   describe('id', () => {
-    it("should return a number", () => {
-      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
+    it('should return a number', () => {
       expect(typeof testEmployee.id).toEqual('number');
-    })
+    });
+    it('calling getId() should return "ID: ###"', () => {
+      const check = 'ID: 281';
+      expect(testEmployee.getId()).toEqual(check);
+    });
   });
   describe('email', () => {
     it("should return a string", () => {
-      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
       expect(typeof testEmployee.email).toEqual('string');
-    })
+    });
+    it('calling getEmail() should return "Email: @@@@@"', () => {
+      const check = 'Email: johnTestEmail@example.com';
+      expect(testEmployee.getEmail()).toEqual(check)
+    });
   });
 })
