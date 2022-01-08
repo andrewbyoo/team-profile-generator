ee1d14aa311dbe9fc23454bac2c5da6b13b6fad0
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-07 19:39:47 -0500

    Added reference to Employee class and added test for initializing the new employee

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require('./Employee');
+
+describe("Employee", () => {
+  describe("Initialize", () => {
+    it("should return an object when called with the 'new' keyword", () => {
+      const testEmployee = new Employee('John', 281, 'johnTestEmail@example.com');
+
+      expect(typeof testEmployee).toEqual('object')
+    })
+  })
+})
