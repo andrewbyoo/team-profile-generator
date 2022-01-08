1020d2cb70d4eb140ede31dcfcaf486a88dd233f
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 13:58:19 -0500

    Added questions array for employee and started writing function to start inquirer

--- index.js
+++ index.js
@@ -5,3 +5,23 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generateHTML');
+
+const employeeQuestions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is the team member\'s name?',
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: 'What is the team member\'s id number?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is the team member\'s email address?',
+  },
+]
+
+const getTeamMembers = function ()
