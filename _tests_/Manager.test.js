e37fbbecf4b609ae02c8a4bd3a64e01897eafebf
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 00:34:07 -0500

    Changed name parameter to give purpose to the name test

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 describe('Manager', () => {
-  const testManager = new Manager('Keanu Reeves', '299', 'keanu@example.com', '3');
+  const testManager = new Manager('keanu reeves', '299', 'keanu@example.com', '3');
   describe('name', () => {
     it('calling getName() should return each name capitalized', () => {
       const check = 'Keanu Reeves';
