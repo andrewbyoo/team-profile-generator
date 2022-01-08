21ba5d97f56c30fc4b9cf27a266ab204aa9dfd33
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 01:16:44 -0500

    Added require aliases

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
-
-const employee = new Employee()
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateHTML = require('./src/generateHTML');
