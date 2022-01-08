88fa6bacae570c428e88903ffce8fd67fed1679a
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-07 21:32:53 -0500

    Added test to check if output is string to name and email

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -5,20 +5,30 @@ describe("Employee", () => {
     it("should return an object when called with the 'new' keyword", () => {
       const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
 
-      expect(typeof testEmployee).toEqual('object')
+      expect(typeof testEmployee).toEqual('object');
     })
   });
   describe('name', () => {
-    it('should return a name capitalized', () => {
-      const check = 'John Wick'
+    it('should return a string', () => {
       const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
-      expect(testEmployee.name).toEqual(check)
+      expect(typeof testEmployee.name).toEqual('string');
+    });
+    it('should return a name capitalized', () => {
+      const check = 'John Wick';
+      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
+      expect(testEmployee.name).toEqual(check);
     });
   });
   describe('id', () => {
     it("should return a number", () => {
       const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
-      expect(typeof testEmployee.id).toEqual('number')
+      expect(typeof testEmployee.id).toEqual('number');
+    })
+  });
+  describe('email', () => {
+    it("should return a string", () => {
+      const testEmployee = new Employee('john wick', '281', 'johnTestEmail@example.com');
+      expect(typeof testEmployee.email).toEqual('string');
     })
   });
 })
