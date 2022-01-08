d79905c10baf319608507314fd43825b20df052f
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 00:36:42 -0500

    Reference to Engineer.js and added Engineer test category and new testEngineer

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,5 @@
+const Engineer = require('../lib/Engineer');
+
+describe('Engineer', () => {
+  const testEngineer = new Engineer('johnny silverhand', '2077', 'silverhand@example.com', 'johnny2077');
+});
