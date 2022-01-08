f7eee7d29c4a8cbddf5beb7d1e412f104b9ddfb6
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 00:45:13 -0500

    Added check for engineer name

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -2,4 +2,10 @@ const Engineer = require('../lib/Engineer');
 
 describe('Engineer', () => {
   const testEngineer = new Engineer('johnny silverhand', '2077', 'silverhand@example.com', 'johnny2077');
+  describe('name', () => {
+    it('calling getName() should return each name capitalized', () => {
+      const check = 'Johnny Silverhand';
+      expect(testEngineer.name).toEqual(check);
+    });
+  });
 });
