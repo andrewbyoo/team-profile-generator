48289311580376efea8f7ddc0a6ae116e8ce51d9
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 01:05:41 -0500

    Name test

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -2,4 +2,10 @@ const Intern = require('../lib/Intern');
 
 describe('Intern', () => {
   const testIntern = new Intern('thomas a anderson', '01101101 01100001 01110100 01110010 01101001 01111000', 'neo@example.com', 'The Matrix');
+  describe('name', () => {
+    it('calling getName() should return each name capitalized', () => {
+      const check = 'Thomas A Anderson';
+      expect(testIntern.name).toEqual(check);
+    });
+  });
 });
