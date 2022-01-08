b5b82593be0b37612f9e6f350d3135d25c234dbd
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 01:04:10 -0500

    Referenced Intern.js and added intern category and defined testIntern

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,5 @@
+const Intern = require('../lib/Intern');
+
+describe('Intern', () => {
+  const testIntern = new Intern('thomas a anderson', '01101101 01100001 01110100 01110010 01101001 01111000', 'neo@example.com', 'The Matrix');
+});
