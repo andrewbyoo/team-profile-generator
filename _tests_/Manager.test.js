df3aa0508078b8094df1865f1cbcf385d6770b20
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-08 00:05:45 -0500

    Removed test to check if the output is a number as leaving the output as a string will not affect the end result in any adverse way

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -3,9 +3,6 @@ const Manager = require('../lib/Manager');
 describe('Manager', () => {
   const testManager = new Manager('Keanu Reeves', '299', 'keanu@example.com', '3');
   describe('office number', () => {
-    it('should return a number', () => {
-      expect(typeof testManager.officeNumber).toEqual('number');
-    });
     it('calling getOfficeNumber() should return "Office Number: ###"', () => {
       const check = 'Office Number: 3';
       expect(testManager.getOfficeNumber()).toEqual(check);
