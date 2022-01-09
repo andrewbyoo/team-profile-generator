c9ecd633dbd715f2d8d425907cabf73f7e010fc8
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-09 04:36:23 -0500

    Added code from template-index.html, replacing the stylesheet link with the link to the style css in the dist folder, split index template code at the cards into 2 functions and exported both functions

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,33 @@
-const generateCard = require('./generateCard');
+const generateBeforeCards = () => {
+  const beforeCards = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Caveat&family=Kalam&family=Vujahday+Script&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./style.css">
+  <title>Team Profiles</title>
+</head>
 
-const generateHTML = data => {
-  return ``
+<header>
+  <h1>My Team</h1>
+</header>
+
+<body>
+  <main class="d-flex flex-wrap p-2 bd-highlight justify-content-center">`;
+  return beforeCards;
 }
 
-module.exports = {generateHTML};
+const generateAfterCards = () => {
+  const afterCards = `  </main>
+</body>
+</html>`;
+  return afterCards;
+}
+
+module.exports = {generateBeforeCards};
+module.exports = {generateAfterCards};
